419f69cfe51b63b3dc1146b2c10107333ad0e2b0
floriscalkoen <floris_calkoen@hotmail.com>
2022-09-12 08:44:17 +0200

    .

---
+++ jupyter/sync-settings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd -P)
+echo "Using $DOTFILES_ROOT as DOTFILES_ROOT"
+set -e
+
+SRC="$HOME/.jupyter/lab/user-settings/"
+DST="$DOTFILES_ROOT/jupyter/lab/user-settings/"
+
+if [[ -d "$SRC" ]];then
+    rsync -a "$SRC" "$DST" --delete
+    echo "sync $SRC with $DST"
+else
+    echo "Cannot find user settings, $SRC does not exist."
+fi
+
