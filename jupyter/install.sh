9980ad50558860b7e337ecb7cd4be9cf127ba11d
floriscalkoen <floris_calkoen@hotmail.com>
2023-01-16 09:50:44 +1100

    updates

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -1,2 +1,14 @@
 #!bin/bash
 
+# install the following packages in the base environment
+mamba install jupyterlab
+mamba install nb_conda_kernels
+mamba install nb_conda
+mamba install jupyter_contrib_nbextensions
+mamba install jupyterlab_code_formatter
+mamba install jupyterlab_vim
+mamba install jupyterlab-git
+mamba install dask_labextension
+
+# in new project environments install
+# mamba install ipykernel
